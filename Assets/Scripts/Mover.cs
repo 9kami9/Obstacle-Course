d84fe4f8a8201e60b50b7e55b8464b6cf8a7b282
dany-falta <36272935+dany-falta@users.noreply.github.com>
2022-08-29 19:17:26 +0300

    asdas

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -22,7 +22,7 @@ public class Mover : MonoBehaviour
     {
         Debug.Log("Welcome to the game!");
         Debug.Log("Move your player with WASD or arrow keys.");
-        Debug.Log("Don't hit the walls!");
+        Debug.Log("Hit the walls!");
     }
 
     void MovePlayer()
